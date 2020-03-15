ed06bc6eb176531570764302e94cd88cc4ae6730
yogesh kathayat <kathayat.yogesh@gmail.com>
2020-03-15 17:17:04 +0400

    Initial commit

---
+++ src/api/routes/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import hobbyRoute from "./hobby.route";
+import userRoute from "./user.route";
+
+
+const router = express.Router();
+
+/**
+ * GET v1/health
+ */
+router.get("/health", (req, res) => res.status(200).send("OK"));
+/**
+ * GET v1/docs
+ */
+router.use("/docs", express.static("docs"));
+
+router.use("/hobbies", hobbyRoute);
+router.use("/users", userRoute);
+
+
+
+
+export default router;
